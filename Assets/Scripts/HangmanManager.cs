3cffdc1c3ce6756066019d3337c301ddd70709ac
SheriMarie <140038384+SheriMarie@users.noreply.github.com>
2023-10-19 16:04:10 +1100

    Dead Ducks now drown when guess letter wrong

--- Assets/Scripts/HangmanManager.cs
+++ Assets/Scripts/HangmanManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,21 @@ public class HangmanManager : MonoBehaviour
 {
     //GAME
     // Have a collection of words that can be used
-    [SerializeField] private string[] wordList;
-    [SerializeField] private string chosenWord;
-    [SerializeField] private char[] lettersOfChosenWord;
-    [SerializeField] private Text[] displayLetters;
+    [SerializeField] private string[] _wordList;
+    [SerializeField] private string _chosenWord;
+    [SerializeField] private char[] _lettersOfChosenWord;
+    [SerializeField] private Text[] _displayLetters;
+    [SerializeField] private Duckie[] _displayDucks;
+    [SerializeField] private int _guesses;
+    private bool gameWon = false;
+    private bool gameLost = false;
+    [Serializable]
+    public struct Duckie
+    {
+        public SpriteRenderer duckAlive;
+        public SpriteRenderer duckDed;
+        public SpriteRenderer duckDrown;
+    }
 
     private void Start()
     {
@@ -20,38 +32,45 @@ public class HangmanManager : MonoBehaviour
     void ChooseWord()
     {
         // Choose a word at random and store it in a variable
-        int randomNumber = Random.Range(0, wordList.Length);
-        chosenWord = wordList[randomNumber];
-        lettersOfChosenWord = chosenWord.ToCharArray();
+        int randomNumber = UnityEngine.Random.Range(0, _wordList.Length);
+        _chosenWord = _wordList[randomNumber];
+        _lettersOfChosenWord = _chosenWord.ToCharArray();
         // Display the length of the word to the user
-        for (int i = 0; i < displayLetters.Length; i++)
+        for (int i = 0; i < _displayLetters.Length; i++)
         {
-            if (i < lettersOfChosenWord.Length)
+            if (i < _lettersOfChosenWord.Length)
             {
-                displayLetters[i].gameObject.SetActive(true);
+                _displayLetters[i].gameObject.SetActive(true);
             }
             else
             {
-                displayLetters[i].gameObject.SetActive(false);
+                _displayLetters[i].gameObject.SetActive(false);
             }
         }
     }
+
+    //Display Visual to user of how many chances left to guess
 
     //Prompt the user to guess a letter
     public void GuessLetter(Text letter)
     {
         //If the guess is correct
-        if (chosenWord.Contains(letter.text))
+        if (_chosenWord.Contains(letter.text))
         {
             //Display guessed letter to user
-            for (int i = 0;i < lettersOfChosenWord.Length; i++)
+            for (int i = 0;i < _lettersOfChosenWord.Length; i++)
             {
-                if (lettersOfChosenWord[i].ToString() == letter.text)
+                if (_lettersOfChosenWord[i].ToString() == letter.text)
                 {
                     letter.gameObject.GetComponentInParent<Image>().color = Color.green;
-                    displayLetters[i].text = letter.text;
+                    _displayLetters[i].text = letter.text;
                 }
             }
+
+            //If correct guess of the word
+            //Tell the user they won
+            //Prompt to play again
+            //Allow user to leave game
 
         }
         //If the guess is incorrect
@@ -60,19 +79,52 @@ public class HangmanManager : MonoBehaviour
             //Display incorrect letter to user 
             letter.gameObject.GetComponentInParent<Image>().color = Color.red;
 
+            if (_guesses > 0)
+            {
+                if (_guesses < 5)
+                {
+                  //  _displayDucks[_guesses.]
+                }
+                //Increment incorrect guesses by 1
+                _guesses--;
+                _displayDucks[_guesses].duckAlive.color = new Color(1,1,1,0);
+                _displayDucks[_guesses].duckDed.color = Color.white;
+                Invoke("DrownDuck", 0.75f);
+            }
+
+            //If the incorrect guesses have all been marked off tell the user 
+            if (_guesses == 0)
+            {
+                letter.gameObject.GetComponentInParent<Button>().interactable = false; 
+                //They lost 
+                // Allow user to exit the program or return to menu
+            }
+
         }
         //block ability to use letter in next guess
         letter.gameObject.GetComponentInParent<Button>().interactable = false;
+
+
+
     }
 
 
+    void DrownDuck()
+    {
+        _displayDucks[_guesses].duckDed.color = new Color(1, 1, 1, 0);
+        _displayDucks[_guesses].duckDrown.color = Color.grey;
+         StartCoroutine(DrownDuckieDrown());
+    }
 
-    //Display Visual to user of how many chances left to guess
-    //If the incorrect guesses have all been marked off tell the user 
-    //They lost 
-    // Allow user to exit the program or return to menu
-    //If correct guess of the word
-    //Tell the user they won
-    //Prompt to play again
-    //Allow user to leave game
+
+    IEnumerator DrownDuckieDrown()
+    {
+
+       while (_displayDucks[_guesses].duckDrown.transform.localPosition.y > -2.6)
+       {
+            _displayDucks[_guesses].duckDrown.transform.position += Vector3.down * 1f * Time.deltaTime;
+            // yield return new WaitForSeconds(.1f);
+            yield return null;
+        }
+    }
 }
